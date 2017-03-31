5c5ad2c684318e39e467d58b00721014dc848852
olivierausseil <olivier.ausseil@sensing-labs.com>
2017-03-31 15:54:36 +0200

    Update ReadHumidityAndTemperatureSMT3x.py

--- ReadHumidityAndTemperatureSMT3x.py
+++ ReadHumidityAndTemperatureSMT3x.py
@@ -3,6 +3,7 @@
 
 
 import time
+
 from smbus2 import SMBus # import library smbus2 compatible to use I2C
 bus = SMBus(1)
 
@@ -10,16 +11,7 @@ I2CAdress = 0x44
 
 bus.write_i2c_block_data(0x44, 0x2C, [0x06])
 
-#time.sleep(0.5)
-
-data = bus.read_i2c_block_data(I2CAdress,0x00,6) # go read the firmware version
-
-#m1 = format(measurements[0],'02x')
-#m2 = format(measurements[1],'02x')
-#m3 = format(measurements[2],'02x')
-#m4 = format(measurements[3],'02x')
-
-#print ("measurements : " + m1 +"."+ m2+"."+ m3+"."+ m4) # display the value
+data = bus.read_i2c_block_data(I2CAdress,0x00,6) 
 
 # Convert the data
 temp = data[0] * 256 + data[1]

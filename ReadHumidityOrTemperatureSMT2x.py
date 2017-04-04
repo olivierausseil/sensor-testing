b5578c57b8ae4e56122743cd608bf175ca4018fa
olivierausseil <olivier.ausseil@sensing-labs.com>
2017-04-04 10:50:28 +0200

    Update ReadHumidityOrTemperatureSMT2x.py

--- ReadHumidityOrTemperatureSMT2x.py
+++ ReadHumidityOrTemperatureSMT2x.py
@@ -31,3 +31,52 @@ humidityPercent = -6 + (125 * humidity / 65535)
 # Output data to screen
 #print "Temperature in Celsius is : %.2f C" %cTemp
 print "Relative Humidity is : %.2f %%RH" %humidityPercent
+#!/usr/bin/python2
+
+from smbus import SMBus
+
+# I2C address of SHT21 ( datasheet_Sensirion_Humidity_Sensors_SHT21_Datasheet_V4.pdf __page 7 __ paragraph 5.3  )
+I2CAdress = 0x40
+
+# command codes ( datasheet_Sensirion_Humidity_Sensors_SHT21_Datasheet_V4.pdf __page 8 __ Table 6 )
+TEM = 0xE3
+HUM = 0xE5
+
+bus = SMBus(1)
+
+# reading raw humidity registers
+raw_humidity = bus.read_word_data(I2CAdress, HUM)
+data_humidity = [
+                    (raw_humidity & 0xFF),
+                    ( (raw_humidity >> 8) & 0xFF ),
+                    ( (raw_humidity >> 16) & 0xFF ),
+                    ( (raw_humidity>>24) & 0xFF )
+                ]
+#print data_humidity
+
+# reading raw temperature registers
+raw_temperature = bus.read_word_data(I2CAdress, TEM)
+data_temperature = [
+                    (raw_temperature & 0xFF),
+                    ( (raw_temperature >> 8) & 0xFF ),
+                    ( (raw_temperature >> 16) & 0xFF ),
+                    ( (raw_temperature>>24) & 0xFF )
+                    ]
+
+#print data_temperature
+
+# compute actual temperature
+#  recover temperature word from raw temperature registers ( datasheet_Sensirion_Humidity_Sensors_SHT21_Datasheet_V4.pdf __page 10 __ paragraph 6 )
+temp = data_temperature[0] * 256 + ( data_temperature[1] & 0xFC )
+#  apply conversion formula ( atasheet_Sensirion_Humidity_Sensors_SHT21_Datasheet_V4.pdf __page 10 __ paragraph 6.1)
+cTemp = -46.85 + (175.72 * temp / 65535.0)
+
+#compute actual humidity
+#  recover humidity word from raw temperature registers ( datasheet_Sensirion_Humidity_Sensors_SHT21_Datasheet_V4.pdf __page 10 __ paragraph 6 )
+humidity = data_humidity[0] * 256 + ( data_humidity[1] & 0xFC )
+#  apply humidity formula (datasheet_Sensirion_Humidity_Sensors_SHT21_Datasheet_V4.pdf __page 10 __ paragraph 6.2 )
+humidityPercent = -6 + (125 * humidity / 65535)
+
+# Output data to screen
+print "Temperature in Celsius is : %.2f C" %cTemp
+print "Relative Humidity is : %.2f %%RH" %humidityPercent
